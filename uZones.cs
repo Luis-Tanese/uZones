6521422c6397b8429691871b748cb5f223166a1d
Luis Tanese <luistanese@gmail.com>
2024-10-04 10:04:29 -0300

    Update uZones.cs

--- uZones.cs
+++ uZones.cs
@@ -8,24 +8,47 @@
     More information can be found here: https://github.com/Luis-Tanese/uZones/wiki/Home.
 */
 
-event onLoad() {
-    loadZonesFromConfig();
-    logger.log("uZones by Tanese V1.3.0 successfully loaded!");
-}
-
 configFilePath = "uZonesConfig.json";
+zoneDataFilePath = "uZonesData.json";
 
 zones = [];
+zoneConfig = {};
+predefinedFlags = [];
+flagPacks = {};
 
-predefinedFlags = ["nodamage", "nobuild", "nocraft", "novehicledamage", "noitemtake", "nocarjack", "nosiphon", "nohook", "nolockpick", "noraid"];
+event onLoad() {
+    loadZoneConfig();
+    loadZonesFromData();
+    logger.log("uZones by Tanese V2.0 successfully loaded!");
+}
 
-flagPacks = {
-    "safezone": ["nodamage", "nobuild", "novehicledamage", "nocarjack", "nosiphon", "nolockpick", "nohook", "noraid"],
-    "nopvpzone": ["nodamage", "novehicledamage"]
-};
+// Functions
 
-function loadZonesFromConfig() {
+function loadZoneConfig() {
     jsonData = file.read(configFilePath);
+    if (jsonData == "") {
+        zoneConfig = {
+            "predefinedFlags": ["nodamage", "nobuild", "nocraft", "novehicledamage"],
+            "flagPacks": {
+                "safezone": ["nodamage", "nobuild", "novehicledamage"]
+            },
+            "customFlags": {}
+        };
+        saveZoneConfig();
+    } else {
+        zoneConfig = deserialize(jsonData);
+        predefinedFlags = zoneConfig["predefinedFlags"];
+        flagPacks = zoneConfig["flagPacks"];
+    }
+}
+
+function saveZoneConfig() {
+    jsonData = zoneConfig.serialize();
+    file.writeAll(configFilePath, jsonData);
+}
+
+function loadZonesFromData() {
+    jsonData = file.read(zoneDataFilePath);
     if (jsonData == "") {
         zones = [];
     } else {
@@ -36,10 +59,13 @@ function loadZonesFromConfig() {
             }
         }
         zones = filterZonesByReady(zones);
-        convertNodesToVector3();
     }
 }
 
+function saveZonesToData() {
+    jsonData = zones.serialize();
+    file.writeAll(zoneDataFilePath, jsonData);
+}
 
 function filterZonesByReady(zones) {
     readyZones = [];
@@ -51,160 +77,49 @@ function filterZonesByReady(zones) {
     return readyZones;
 }
 
-function saveZonesToConfig() {
-    jsonData = zones.serialize();
-    file.writeAll(configFilePath, jsonData);
+function addPlayerToZone(zone, playerId) {
+    if (!zone.containsKey("players")) {
+        zone["players"] = [];
+    }
+    if (!zone["players"].contains(playerId)) {
+        zone["players"].add(playerId);
+    }
 }
 
-function isPointInZone(playerPos, zoneNodes) {
-    crossings = 0;
-    for (i = 0; i < zoneNodes.count; i++) {
-        count = i + 1;
-        nextIndex = count % zoneNodes.count;
-        node1 = zoneNodes[i];
-        node2 = zoneNodes[nextIndex];
-        isNode1Above = node1["z"] > playerPos.z;
-        isNode2Above = node2["z"] > playerPos.z;
-        if (isNode1Above != isNode2Above) {
-            zDiffPlayerNode1 = playerPos.z - node1["z"];
-            xDiffNode2Node1 = node2["x"] - node1["x"];
-            zDiffNode2Node1 = node2["z"] - node1["z"];
-            intersectXNumerator = zDiffPlayerNode1 * xDiffNode2Node1;
-            intersectXDenominator = zDiffNode2Node1;
-            if (math.abs(intersectXDenominator) > 0) {
-                intersectXIndex = intersectXNumerator / intersectXDenominator;
-                intersectX = node1["x"] + intersectXIndex;
-                if (playerPos.x < intersectX) {
-                    crossings += 1;
-                }
-            }
+function removePlayerFromZone(zone, playerId) {
+    if (zone.containsKey("players")) {
+        zone["players"].remove(playerId);
+    }
+}
+
+function filterEnteredZones(currentZones, previousZones) {
+    result = [];
+    foreach (zone in currentZones) {
+        if (!previousZones.contains(zone)) {
+            result.add(zone);
         }
     }
-    womp = false;
-    wompity = crossings % 2;
-    if (wompity == 1) {
-        womp = true;
-    }
-    return womp;
+    return result;
 }
 
-function checkPlayerZone(player) {
-    playerPos = player.position;
+function filterExitedZones(previousZones, currentZones) {
+    result = [];
+    foreach (zone in previousZones) {
+        if (!currentZones.contains(zone)) {
+            result.add(zone);
+        }
+    }
+    return result;
+}
+
+function checkPlayerZone(playerPos) {
     insideZones = [];
     foreach (zone in zones) {
-        if (zone["isReady"] == true and isPointInZone(playerPos, zone["nodes"])) {
-            insideZones.add(zone["zoneName"]);
+        if (zone["isReady"] and isPointInZone(playerPos, zone["nodes"])) {
+            insideZones.add(zone);
         }
     }
     return insideZones;
-}
-
-function getZonesEntered(currentZones, previousZones) {
-    enteredZones = [];
-    foreach (zone in currentZones) {
-        if (!previousZones.contains(zone)) {
-            enteredZones.add(zone);
-        }
-    }
-    return enteredZones;
-}
-
-function getZonesExited(currentZones, previousZones) {
-    exitedZones = [];
-    foreach (zone in previousZones) {
-        if (!currentZones.contains(zone)) {
-            exitedZones.add(zone);
-        }
-    }
-    return exitedZones;
-}
-
-event onPlayerPositionUpdated(player) {
-    currentZones = checkPlayerZone(player);
-    previousZones = player.getData("zone");
-    if (previousZones == null) {
-        previousZones = [];
-    }
-    enteredZones = getZonesEntered(currentZones, previousZones);
-    exitedZones = getZonesExited(currentZones, previousZones);
-    foreach (zone in enteredZones) {
-        logger.log("{0} has entered zone: {1}".format(player.name, zone));
-        player.message("You entered {0}".format(zone), "green");
-    }
-    foreach (zone in exitedZones) {
-        logger.log("{0} has left zone: {1}".format(player.name, zone));
-        player.message("You left {0}".format(zone), "red");
-    }
-    player.setData("zone", currentZones);
-}
-
-
-function convertNodesToVector3() {
-    foreach (zone in zones) {
-        foreach (node in zone["nodes"]) {
-            node["x"] = node["x"];
-            node["y"] = node["y"];
-            node["z"] = node["z"];
-        }
-    }
-}
-
-command uzones() {
-    permission = "uZones.manage";
-    execute() {
-        if (arguments.count < 1) {
-            player.message("Error: Missing action argument.", "red");
-            return;
-        }
-        action = arguments[0];
-        if (action == "add") {
-            if (arguments.count < 3) {
-                player.message("Error: Missing type or zoneName argument.", "red");
-                return;
-            }
-            handleAdd(player, arguments[1], arguments[2], arguments[3]);
-        } else if (action == "remove") {
-            if (arguments.count < 3) {
-                player.message("Error: Missing type or zoneName argument.", "red");
-                return;
-            }
-            handleRemove(player, arguments[1], arguments[2], arguments[3]);
-        } else if (action == "replace") {
-            if (arguments.count < 4) {
-                player.message("Error: Missing type, zoneName, or new value argument.", "red");
-                return;
-            }
-            handleReplace(player, arguments[1], arguments[2], arguments[3]);
-        } else if (action == "list") {
-            if (arguments.count < 2) {
-                player.message("Error: Missing type argument.", "red");
-                return;
-            }
-            handleList(player, arguments[1], arguments[2]);
-        } else if (action == "tp") {
-            if (arguments.count < 3) {
-                player.message("Error: Missing zoneName or nodeNumber argument.", "red");
-                return;
-            }
-            handleTeleport(player, arguments[1], arguments[2]);
-        } else if (action == "getpos") {
-            handleGetPos(player);
-        } else if (action == "visualize") {
-            if (arguments.count < 3) {
-                player.message("Error: Missing type or zoneName argument.", "red");
-                return;
-            }
-            handleVisualize(player, arguments[1], arguments[2], arguments[3]);
-        } else if (action == "json") {
-            if (arguments[1] != "refresh") {
-                player.message("Error: Missing refresh argument.", "red");
-                return;
-            }
-            loadZonesFromConfig();
-        } else {
-            player.message("Error: Unknown action.", "red");
-        }
-    }
 }
 
 function handleAdd(player, type, zoneName, flag) {
@@ -215,7 +130,7 @@ function handleAdd(player, type, zoneName, flag) {
             return;
         }
         zones.add({ "zoneName": zoneName, "nodes": [], "isReady": false });
-        saveZonesToConfig();
+        saveZonesToData();
         player.message("Zone {0} added. Add atleast 3 nodes for the zone to be ready.".format(zoneName), "green");
     } else if (type == "node") {
         if (zone == null) {
@@ -232,9 +147,8 @@ function handleAdd(player, type, zoneName, flag) {
             zone["isReady"] = true;
             player.message("Zone {0} is now ready.".format(zoneName), "green");
         }
-        saveZonesToConfig();
+        saveZonesToData();
         player.message("Node added to zone {0}.".format(zoneName), "green");
-    }
     } else if (type == "flag") {
         if (zone == null) {
             player.message("Zone not found!", "red");
@@ -251,7 +165,7 @@ function handleAdd(player, type, zoneName, flag) {
         if (predefinedFlags.contains(flag)) {
             if (!zone["flags"].contains(flag)) {
                 zone["flags"].add(flag);
-                saveZonesToConfig();
+                saveZonesToData();
                 player.message("Flag {0} added to zone {1}.".format(flag, zoneName), "green");
             } else {
                 player.message("Flag {0} already exists in zone {1}.".format(flag, zoneName), "yellow");
@@ -262,7 +176,7 @@ function handleAdd(player, type, zoneName, flag) {
                     zone["flags"].add(flagInPack);
                 }
             }
-            saveZonesToConfig();
+            saveZonesToData();
             player.message("Flag pack {0} added to zone {1}.".format(flag, zoneName), "green");
         }
     }
@@ -276,7 +190,7 @@ function handleRemove(player, type, zoneName, nodeIndex) {
             return;
         }
         zones.remove(zone);
-        saveZonesToConfig();
+        saveZonesToData();
         player.message("Zone {0} removed.".format(zoneName), "green");
     } else if (type == "node" and nodeIndex != null) {
         if (zone == null or nodeIndex.toNumber() >= zone["nodes"].count) {
@@ -284,7 +198,7 @@ function handleRemove(player, type, zoneName, nodeIndex) {
             return;
         }
         zone["nodes"].removeAt(nodeIndex.toNumber());
-        saveZonesToConfig();
+        saveZonesToData();
         player.message("Node {0} removed from zone {1}.".format(nodeIndex, zoneName), "green");
     } else if (type == "flag" and nodeIndex != null) {
         if (zone == null) {
@@ -300,7 +214,7 @@ function handleRemove(player, type, zoneName, nodeIndex) {
             return;
         }
         zone["flags"].remove(nodeIndex);
-        saveZonesToConfig();
+        saveZonesToData();
         player.message("Flag {0} removed from zone {1}.".format(nodeIndex, zoneName), "green");
     } else {
         player.message("Error: Missing node number or flag for removal.", "red");
@@ -315,7 +229,7 @@ function handleReplace(player, type, zoneName, newValue) {
             return;
         }
         zone["zoneName"] = newValue;
-        saveZonesToConfig();
+        saveZonesToData();
         player.message("Zone {0} renamed to {1}.".format(zoneName, newValue), "green");
     } else if (type == "node") {
         zone = getZoneByName(zoneName);
@@ -328,7 +242,7 @@ function handleReplace(player, type, zoneName, newValue) {
         node["x"] = player.position.x;
         node["y"] = player.position.y;
         node["z"] = player.position.z;
-        saveZonesToConfig();
+        saveZonesToData();
         player.message("Node {0} in zone {1} replaced.".format(nodeIndex, zoneName), "green");
     }
 }
@@ -452,6 +366,58 @@ function zoneHasFlag(player, flag) {
     return womp;
 }
 
+function filterArray(array, predicate) {
+    result = [];
+    foreach (item in array) {
+        if (predicate(item)) {
+            result.add(item);
+        }
+    }
+    return result;
+}
+
+function hasOverridePermission(player, flag) {
+    return player.hasPermission("uZones.override." + flag);
+}
+
+function isObjectInZone(position) {
+    insideZones = [];
+    foreach (zone in zones) {
+        if (zone["isReady"] == true and isPointInZone(position, zone["nodes"])) {
+            insideZones.add(zone["zoneName"]);
+        }
+    }
+    return insideZones;
+}
+
+function zoneHasFlagForZone(zoneName, flag) {
+    zone = getZoneByName(zoneName);
+    if (zone != null and zone.containsKey("flags") and zone["flags"].contains(flag)) {
+        return true;
+    }
+    return false;
+}
+
+// Events
+
+event onPlayerPositionUpdated(player) {
+    playerPos = player.position;
+    insideZones = checkPlayerZone(playerPos);
+    previousZones = player.getData("zone");
+    if (previousZones == null) {
+        previousZones = [];
+    }
+    enteredZones = filterEnteredZones(currentZones, previousZones);
+    exitedZones = filterExitedZones(previousZones, currentZones);
+    foreach (zone in enteredZones) {
+        addPlayerToZone(zone, player.id);
+    }
+    foreach (zone in exitedZones) {
+        removePlayerFromZone(zone, player.id);
+    }
+    player.setData("zone", insideZones);
+    saveZonesToData();
+}
 
 event onBarricadeBuild(player, id, position, cancel) {
     if (zoneHasFlag(player, "nobuild") and !hasOverridePermission(player, "nobuild")) {
@@ -561,34 +527,62 @@ event onVehicleDamaged(vehicle, player, cause, damage, cancel) {
     }
 }
 
-function filterArray(array, predicate) {
-    result = [];
-    foreach (item in array) {
-        if (predicate(item)) {
-            result.add(item);
+// Command
+
+command uzones() {
+    permission = "uZones.manage";
+    execute() {
+        if (arguments.count < 1) {
+            player.message("Error: Missing action argument.", "red");
+            return;
+        }
+        action = arguments[0];
+        if (action == "add") {
+            if (arguments.count < 3) {
+                player.message("Error: Missing type or zoneName argument.", "red");
+                return;
+            }
+            handleAdd(player, arguments[1], arguments[2], arguments[3]);
+        } else if (action == "remove") {
+            if (arguments.count < 3) {
+                player.message("Error: Missing type or zoneName argument.", "red");
+                return;
+            }
+            handleRemove(player, arguments[1], arguments[2], arguments[3]);
+        } else if (action == "replace") {
+            if (arguments.count < 4) {
+                player.message("Error: Missing type, zoneName, or new value argument.", "red");
+                return;
+            }
+            handleReplace(player, arguments[1], arguments[2], arguments[3]);
+        } else if (action == "list") {
+            if (arguments.count < 2) {
+                player.message("Error: Missing type argument.", "red");
+                return;
+            }
+            handleList(player, arguments[1], arguments[2]);
+        } else if (action == "tp") {
+            if (arguments.count < 3) {
+                player.message("Error: Missing zoneName or nodeNumber argument.", "red");
+                return;
+            }
+            handleTeleport(player, arguments[1], arguments[2]);
+        } else if (action == "getpos") {
+            handleGetPos(player);
+        } else if (action == "visualize") {
+            if (arguments.count < 3) {
+                player.message("Error: Missing type or zoneName argument.", "red");
+                return;
+            }
+            handleVisualize(player, arguments[1], arguments[2], arguments[3]);
+        } else if (action == "json") {
+            if (arguments[1] != "refresh") {
+                player.message("Error: Missing refresh argument.", "red");
+                return;
+            }
+            loadZoneConfig();
+        } else {
+            player.message("Error: Unknown action.", "red");
         }
     }
-    return result;
-}
-
-function hasOverridePermission(player, flag) {
-    return player.hasPermission("uZones.override." + flag);
-}
-
-function isObjectInZone(position) {
-    insideZones = [];
-    foreach (zone in zones) {
-        if (zone["isReady"] == true and isPointInZone(position, zone["nodes"])) {
-            insideZones.add(zone["zoneName"]);
-        }
-    }
-    return insideZones;
-}
-
-function zoneHasFlagForZone(zoneName, flag) {
-    zone = getZoneByName(zoneName);
-    if (zone != null and zone.containsKey("flags") and zone["flags"].contains(flag)) {
-        return true;
-    }
-    return false;
 }
